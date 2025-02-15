8ee2f0ae09e6867da7426d26ee9c2d74ac9870ef
Abdul Muaid <abdulmuaid786@gmail.com>
2025-02-15 17:16:18 +0500

    Playing with request handler

--- src/app.js
+++ src/app.js
@@ -5,34 +5,17 @@ app.listen(7777,()=>{
  console.log("Server is successfully listening on 7777")
 });
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Abdul", lastName: "Muaid"})
-});
-
-app.post("/user",(req,res)=>{
-    res.send("Data has Successfully Stored in Database!!!")
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Data has been Deleted From Database!!")
-});
-
-app.use("/test", (req, res)=>{
- res.send("Hello World!!, From Test")
-});
-
-app.use("/home", (req, res)=>{
- res.send("Hello World!!, From Home")    
-});
-
-app.use("/index", (req, res)=>{
-    app.use((req, res)=>{
-        res.send("Hello World!!")
-        
-});  res.send("Hello World!!, From Index")        
-});
-        
-app.use((req, res)=>{
-    res.send("Hello World!!")    
-   });  
-      
+app.get("/user",(req,res,next)=>{
+   console.log("From the First Request Handler");
+    //res.send({firstName:"Abdul", lastName: "Muaid"})
+    next();
+}),
+app.get("/user",(req,res,next)=>{
+    console.log("From 2nd Request Handler");
+     //res.send({firstName:"Abdul", lastName: "Muaid", Response:"2nd"})
+     next();
+ }),
+app.get("/user",(req,res,next)=>{
+    console.log("From the 3rd Request Handler");
+     res.send({firstName:"Abdul", lastName: "Muaid", Response:"3rd"})     
+ });
