457d6893473656925eab6985f9c847e5a97cae55
Abdul Muaid <abdulmuaid786@gmail.com>
2025-02-24 17:35:41 +0500

    Encrypted-password-into-passwordHash

--- src/app.js
+++ src/app.js
@@ -1,106 +1,108 @@
-const express = require('express');
-const { connectDB } = require('./config/database');
+const express = require("express");
+const { connectDB } = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
 const app = express();
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-   const user = new User(req.body);
-   try {
-      await user.save();
-      res.send("User Added Successfully");
-   }
-   catch (err) {
-      res.status(400).send("Error saving User Data" + err.message);
+  try {
+    // Validation of Data
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
 
-   }
+    // Encrypt the Password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
 
+    // Creating a new instance of the user Model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
 app.get("/user", async (req, res) => {
-   const userEmail = req.body.emailId;
+  const userEmail = req.body.emailId;
 
-   try {
-      const users = await User.find({ emailId: userEmail });
-      if (users.length === 0) {
-         res.status(404).send("User Not Found");
-      }
-      else {
-         res.send(users);
-      }
-   } catch (err) {
-      res.status(400).send("Something went wrong");
-   }
-
-
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 app.get("/feed", async (req, res) => {
-   try {
-      const users = await User.find({});
-      res.send(users);
-
-   } catch (err) {
-      res.status(400).send("Something went wrong");
-   }
-
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 app.delete("/user", async (req, res) => {
-   const userId = req.body.userId;
+  const userId = req.body.userId;
 
-   try {
-      const user = await User.findByIdAndDelete(userId);
+  try {
+    const user = await User.findByIdAndDelete(userId);
 
-      res.send("User Delete Successfully!")
-
-   } catch (err) {
-      res.status(400).send("Something went wrong");
-   }
-
-
+    res.send("User Delete Successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 app.patch("/user", async (req, res) => {
-   const userId = req.body.userId;
-   const data = req.body;
+  const userId = req.body.userId;
+  const data = req.body;
 
-   try {
-      const users = await User.findByIdAndUpdate({ _id: userId }, data, { runvalidation: true,});
-      // API's Validation 
-      const ALLOWED_UPDATES = ["userId","photoUrl", "about", "age", "skills"];
-      const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k)); 
-       
-      if(!isUpdateAllowed){
-         throw new Error("Update Not Allowed");
-      }
+  try {
+    const users = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runvalidation: true,
+    });
+    // API's Validation
+    const ALLOWED_UPDATES = ["userId", "photoUrl", "about", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
 
-      if(data?.skills.length > 10){
-         throw new Error("Skills cannot be over 10!");
-         
-      }
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not Allowed");
+    }
 
-      res.send("User Updatad Succsessfully!")
-   } catch (err) {
-      res.status(400).send("Updated Failed: " + err.message);
-   }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be over 10!");
+    }
 
+    res.send("User Updatad Succsessfully!");
+  } catch (err) {
+    res.status(400).send("Updated Failed: " + err.message);
+  }
 });
 
 connectDB()
-   .then(() => {
-      console.log("Database connection established...");
-      app.listen(7777, () => {
-         console.log("Server is successfully listening on 7777")
-      });
-
-   })
-   .catch((err) => {
-      console.err("Datbase cannot be connected!")
-   });
-
-
-
-
-
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on 7777");
+    });
+  })
+  .catch((err) => {
+    console.err("Datbase cannot be connected!");
+  });
